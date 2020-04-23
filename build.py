c1857ac310080c4d8e6302fd6bfef6d29d4dce75
Jed Fox <git@twopointzero.us>
2020-04-22 20:10:22 -0400

    New data (they switched things)

--- build.py
+++ build.py
@@ -7,24 +7,22 @@ from datetime import date
 import os
 
 driver = webdriver.Safari()
-driver.get('https://app.powerbigov.us/view?r=eyJrIjoiMDQ5YzRhNzktZTRiNS00YjFkLWFiMGItYzliOWQ2MDNmODExIiwidCI6IjJkMGYxZGI2LWRkNTktNDc3Mi04NjVmLTE5MTQxNzVkMDdjMiJ9')
+driver.get('https://ri-department-of-health-covid-19-data-rihealth.hub.arcgis.com')
 
-read_number = lambda selector: int(driver.find_element_by_css_selector(selector).text.replace(',',''))
+read_number = lambda title: int(driver.find_element_by_css_selector("[title='" + title + "'] ~ .text-left > .ss-value").text.replace(',',''))
 
 dict_key_values = {}
 try:
-    WebDriverWait(driver, 10).until(lambda x: driver.find_element_by_css_selector("visual-container-modern:nth-child(10) title"))
-    dict_key_values['total_negative'] = read_number("visual-container-modern:nth-child(10) title")
-    dict_key_values['total_fatalities'] = read_number("visual-container-modern:nth-child(7) title")
-    dict_key_values['total_positive'] = read_number("visual-container-modern:nth-child(9) title")
-    dict_key_values['new_cases'] = read_number("visual-container-modern:nth-child(8) title")
+    WebDriverWait(driver, 10).until(lambda x: driver.find_element_by_css_selector("[title='Covid-19 Negative '] ~ .text-left > .ss-value:not(:empty)"))
+    dict_key_values['total_negative'] = read_number("Covid-19 Negative ")
+    dict_key_values['total_fatalities'] = read_number("COVID-19 Fatalities")
+    dict_key_values['total_positive'] = read_number("Total COVID-19 Positive")
+    dict_key_values['new_cases'] = read_number("Newly Reported Positive Tests ")
 
-    WebDriverWait(driver, 10).until(lambda x: driver.find_element_by_css_selector("visual-container-modern:nth-child(11) .cardItemContainer:nth-child(1) > .caption"))
-
-    dict_key_values['hospitalized'] = read_number("visual-container-modern:nth-child(11) .cardItemContainer:nth-child(1) > .caption")
-    dict_key_values['icu'] = read_number("visual-container-modern:nth-child(11) .cardItemContainer:nth-child(2) > .caption")
-    dict_key_values['vent'] = read_number("visual-container-modern:nth-child(11) .cardItemContainer:nth-child(3) > .caption")
-    dict_key_values['discharged'] = read_number("visual-container-modern:nth-child(11) .cardItemContainer:nth-child(4) > .caption")
+    dict_key_values['hospitalized'] = read_number(" Currently Hospitalized")
+    dict_key_values['icu'] = read_number("Currently in ICU")
+    dict_key_values['vent'] = read_number("Currently on Vent")
+    # dict_key_values['discharged'] = read_number("visual-container-modern:nth-child(11) .cardItemContainer:nth-child(4) > .caption")
 
     filename = 'archive/' + str(date.today()) + '.csv'
     with open(filename, 'w') as f:
@@ -32,5 +30,7 @@ try:
             f.write("%s,%s\n"%(key,dict_key_values[key]))
 
     print(os.system('cp -f ' + filename + ' latest.csv'))
+except Exception as e:
+    print(e)
 finally:
     driver.quit()
